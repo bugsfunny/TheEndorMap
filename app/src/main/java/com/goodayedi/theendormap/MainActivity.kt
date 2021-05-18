2b6bf2f3eca810c8ccca471433f68cd0dd3e3313
Bugsfunny <js.denize@gmail.com>
2021-05-18 22:14:31 +0200

    Get last location

--- app/src/main/java/com/goodayedi/theendormap/MainActivity.kt
+++ app/src/main/java/com/goodayedi/theendormap/MainActivity.kt
@@ -2,25 +2,36 @@ package com.goodayedi.theendormap
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import timber.log.Timber
 
 private const val REQUEST_LOCATION_PERMISSION_LAST_LOCATION = 1
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         updateLocation()
     }
 
     private fun updateLocation() {
-        if (checkLocationPermission()) {
+        if (!checkLocationPermission()) {
             return
         }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                Timber.i("$location")
+            }
     }
 
     private fun checkLocationPermission(): Boolean {
