aea0fd5e8b05668288f4372abfa5a744f976cb61
Bugsfunny <js.denize@gmail.com>
2021-05-18 21:26:26 +0200

    Check Permissions

--- app/src/main/java/com/goodayedi/theendormap/MainActivity.kt
+++ app/src/main/java/com/goodayedi/theendormap/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.goodayedi.theendormap
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+private const val REQUEST_LOCATION_PERMISSION_LAST_LOCATION = 1
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        updateLocation()
+    }
+
+    private fun updateLocation() {
+        if (checkLocationPermission()) {
+            return
+        }
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION_LAST_LOCATION
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            return
+        }
+        when(requestCode){
+            REQUEST_LOCATION_PERMISSION_LAST_LOCATION -> updateLocation()
+        }
     }
 }
